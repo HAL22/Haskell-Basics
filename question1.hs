c5421baf2f8a6c2db105f7baa938bc36450458c2
Thethela Faltein <thethelafaltein@gmail.com>
2019-03-02 14:35:19 +0200

    question 1 is more general

--- question1.hs
+++ question1.hs
@@ -1,5 +1,9 @@
 -- Thethela Faltein
 
-mult:: ([Integer]) -> (Integer)
+--mult:: ([Integer]) -> (Integer)
+--mult [] = 1
+--mult(x:xs) = x * mult(xs)
+
+mult:: Num a => [a] -> a
 mult [] = 1
-mult(x:xs) = x * mult(xs)
+mult(x:xs) = x * mult xs
