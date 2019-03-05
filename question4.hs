749920611f3371698bcec69d7d7e67fcb01fc912
Thethela Faltein <thethelafaltein@gmail.com>
2019-03-05 13:59:13 +0200

    finished question 4, but not sure the difference between guarderd equations and pattern matching

--- question4.hs
+++ question4.hs
@@ -1,2 +1,19 @@
 -- Question 4
 
+-- 1
+
+safetail_1:: [a] -> [a]
+safetail_1 xs = if null xs then []
+	else tail xs
+
+--2
+	
+safetail_2:: [a] -> [a]
+safetail_2 [] = []
+safetail_2 xs = tail xs
+
+--3
+
+safetail_3:: [a] -> [a]
+safetail_3 [] = []
+safetail_3 (x:xs) = xs
